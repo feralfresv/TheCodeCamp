f67958167df142cfc9c88d470e128383ed380ca8
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-03-27 01:49:32 -0600

    6 - Functional APIs

---
+++ starting/TheCodeCamp/Controllers/OperationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationController : ApiController
+    {
+        [HttpOptions]
+        [Route("api/refreshconfig")]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok("refresh_done");
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);             
+            }
+        }
+    }
+}
